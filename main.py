f866253c686d5eb8d0ff8b52b181df2d2fcc1b54
bbnnaz@gmail.com <bbnnaz@gmail.com>
2022-02-17 18:32:30 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import csv
+import glob
+import os
+
+import requests
+from bs4 import BeautifulSoup
+
+def bb_set_urls():
+    global url, raw, module, raw_formatter
+    url = ""
+    raw = "/raw"
+    module = ""
+    raw_formatter = "?at=refs%2Fheads%2Fmaster"
+
+
+
+def bb_url_former():
+
+
+def parse_bb():
+
+
+def comment_analyser():
+
+
+def tf_file_list():
+
+
+def from_github():
+
+
+def bb_call():
+
+
+if __name__ == '__main__':
+    print("")
